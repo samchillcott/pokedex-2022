86e4ffe939f28acbbcb6b031969bd29ad80af786
samchillcott1 <samchillcott@gmail.com>
2022-10-20 11:05:05 +0100

    add basic loading state

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import axios from 'axios'
 
 function App() {
   const [pokemon, setPokemon] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon')
+    setLoading(true)
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
       .then((response) => {
+        setLoading(false)
         setPokemon(response.data.results.map(p => p.name))
       })
       .catch((error) => console.log(error))
   }, [])
+
+  if (loading) return "Loading results.."
 
   return (
     <div className="App">
---
+++ src/PokemonDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PokemonDetail = ({p}) => {
+  return (
+    <div>Name: {p.name}</div>
+    <div>Image</div>
+    <div>Description: {p.</div>
+    <div>Other Characteristic</div>
+  )
+}
+
+export default PokemonDetail
